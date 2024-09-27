91d1b9ab429a54df82d1b3418c0d3edc99850f5a
jzafvt771 <ijmm7171@gmail.com>
2024-09-27 04:47:59 -0500

    OnboardingIMages start

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,14 +1,24 @@
+import { onBoardingImages } from '@/constants'
 import { useTheme } from '@/context/ThemeContext'
 import React from 'react'
-import { Text } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { ImageBackground, Text, View } from 'react-native'
 
 const Welcome = () => {
     const { isDark } = useTheme()
+
+    console.log(onBoardingImages[0])
     return (
-        <SafeAreaView className={`flex flex-1 justify-center items-center ${isDark ? "bg-rose-500 " : "bg-white-100 "} `}>
-            <Text className={`font-ralewayBold text-3xl text-white-100 ${isDark ? "text-white-100" : "text-darkGray-500"}`}>Welcome Poligym s</Text>
-        </SafeAreaView>
+        <ImageBackground
+            source={isDark ? onBoardingImages[0] : onBoardingImages[1]}
+            className="flex-1 justify-center items-center w-full h-full"
+            resizeMode="cover" 
+        >
+            <View className="flex-1 justify-center items-center ">
+                <Text className={`font-ralewayBold text-3xl`}>
+                    Welcome Poligym
+                </Text>
+            </View>
+        </ImageBackground>
     )
 }
 
---
+++ constants/index.ts
@@ -0,0 +1,8 @@
+
+const onboardingImgD1 = require('@/assets/images/dk-ob1.jpg')
+const onboardingImgL1 = require('@/assets/images/lg-ob1.jpg')
+
+export const onBoardingImages = [
+    onboardingImgD1,
+    onboardingImgL1,
+]
